21b0b0c38545490e51538de77ec1ea6f19a9e488
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-09 20:28:44 +0300

    Add(application): add CreateVideoCommand, handler, and AutoMapper profile

---
+++ AIYTVideoSummarizer.Application/Handlers/VideoHandlers/CreateVideoCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AIYTVideoSummarizer.Application.Commands.VideoCommands;
+using AIYTVideoSummarizer.Application.DTOs.VideoDtos;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AIYTVideoSummarizer.Domain.Entities;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.VideoHandlers
+{
+    public class CreateVideoCommandHandler : IRequestHandler<CreateVideoCommand,CreateVideoDto>
+    {
+        private readonly IVideoRepository _videoRepository;
+        private readonly IMapper _mapper;
+
+        public CreateVideoCommandHandler(IVideoRepository videoRepository,IMapper mapper)
+        {
+            _videoRepository = videoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreateVideoDto> Handle(CreateVideoCommand request, CancellationToken cancellationToken)
+        {
+            var video = _mapper.Map<Video>(request);
+            video.Id = Guid.NewGuid();
+            var addedVideo = await _videoRepository.AddAsync(video);
+            return _mapper.Map<CreateVideoDto>(addedVideo);
+        }
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Profiles/VideoProfile.cs
@@ -0,0 +1,19 @@
+﻿
+
+using AIYTVideoSummarizer.Application.Commands.VideoCommands;
+using AIYTVideoSummarizer.Application.DTOs.VideoDtos;
+using AIYTVideoSummarizer.Domain.Entities;
+using AutoMapper;
+
+namespace AIYTVideoSummarizer.Application.Profiles
+{
+    public class VideoProfile:Profile
+    {
+        public VideoProfile()
+        {
+            CreateMap<Video, VideoDto>();
+            CreateMap<Video, CreateVideoDto>();
+            CreateMap<CreateVideoCommand, Video>();
+        }
+    }
+}
