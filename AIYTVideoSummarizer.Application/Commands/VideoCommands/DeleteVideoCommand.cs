81384e91e3d3c80a4604b2f2a7ffc2b18036040a
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-10 00:04:16 +0300

    Add(application): add DeleteVideoCommand and handler

---
+++ AIYTVideoSummarizer.Application/Commands/VideoCommands/DeleteVideoCommand.cs
@@ -0,0 +1,11 @@
+﻿
+using AIYTVideoSummarizer.Application.DTOs.VideoDtos;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Commands.VideoCommands
+{
+    public class DeleteVideoCommand:IRequest<VideoDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Handlers/VideoHandlers/DeleteVideoCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿
+using AIYTVideoSummarizer.Application.Commands.VideoCommands;
+using AIYTVideoSummarizer.Application.DTOs.VideoDtos;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.VideoHandlers
+{
+    public class DeleteVideoCommandHandler : IRequestHandler<DeleteVideoCommand, VideoDto>
+    {
+        private readonly IVideoRepository _videoRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteVideoCommandHandler(IVideoRepository videoRepository,IMapper mapper)
+        {
+            _videoRepository = videoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<VideoDto> Handle(DeleteVideoCommand request, CancellationToken cancellationToken)
+        {
+
+            var video = await _videoRepository.GetByIdAsync(request.Id) ?? throw new KeyNotFoundException($"Video with ID {request.Id} was not found.");
+            await _videoRepository.DeleteAsync(request.Id);
+            return _mapper.Map<VideoDto>(video);
+        }
+    }
+}
