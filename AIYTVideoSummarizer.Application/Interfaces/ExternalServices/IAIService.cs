323e8332e45eb37eba7390b643fe15848a00f246
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-10 22:31:53 +0300

    Add(application): external service IAIService interface added

---
+++ AIYTVideoSummarizer.Application/Interfaces/ExternalServices/IAIService.cs
@@ -0,0 +1,11 @@
+﻿
+using AIYTVideoSummarizer.Application.DTOs.VideoDtos;
+
+namespace AIYTVideoSummarizer.Application.Interfaces.ExternalServices
+{
+    public interface IAIService
+    {
+        Task<string> GetYTVideoId(string videoUrl);
+        Task<VideoSummaryResponseDto> SummarizeVideo(string videoUrl,string promptName);
+    }
+}
