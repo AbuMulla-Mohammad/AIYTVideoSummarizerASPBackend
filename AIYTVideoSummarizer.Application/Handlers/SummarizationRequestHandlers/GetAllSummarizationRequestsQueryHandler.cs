289ed735050909f1febfdc0b610accb6edaca84d
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-13 19:30:25 +0300

    Add(application: GetAllSummarizationRequest Query, Handler

---
+++ AIYTVideoSummarizer.Application/Handlers/SummarizationRequestHandlers/GetAllSummarizationRequestsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿
+using AIYTVideoSummarizer.Application.DTOs.SummarizationRequestDtos;
+using AIYTVideoSummarizer.Application.Queries.SummarizationRequestQueries;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.SummarizationRequestHandlers
+{
+    public class GetAllSummarizationRequestsQueryHandler : IRequestHandler<GetAllSummarizationRequestsQuery, List<SummarizationRequestDto>>
+    {
+        private readonly ISummarizationRequestRepository _summarizationRequestRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllSummarizationRequestsQueryHandler(
+            ISummarizationRequestRepository summarizationRequestRepository,
+            IMapper mapper)
+        {
+            _summarizationRequestRepository = summarizationRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<SummarizationRequestDto>> Handle(GetAllSummarizationRequestsQuery request, CancellationToken cancellationToken)
+        {
+            var summarizationRequests = await _summarizationRequestRepository.GetAllAsync();
+            return _mapper.Map<List<SummarizationRequestDto>>(summarizationRequests);
+        }
+    }
+}
