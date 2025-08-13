68388aae2a4f9ff46b498c59beba7ba52a22fbbd
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-13 19:20:04 +0300

    Add(application): GetSummarizationRequestsByStatus Query,Handler

---
+++ AIYTVideoSummarizer.Application/Handlers/SummarizationRequestHandlers/GetSummarizationRequestsByStatusQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿
+using AIYTVideoSummarizer.Application.DTOs.SummarizationRequestDtos;
+using AIYTVideoSummarizer.Application.Queries.SummarizationRequestQueries;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.SummarizationRequestHandlers
+{
+    public class GetSummarizationRequestsByStatusQueryHandler : IRequestHandler<GetSummarizationRequestsByStatusQuery, List<SummarizationRequestDto>>
+    {
+        private readonly ISummarizationRequestRepository _summarizationRequestRepository;
+        private readonly IMapper _mapper;
+
+        public GetSummarizationRequestsByStatusQueryHandler(
+            ISummarizationRequestRepository summarizationRequestRepository,
+            IMapper mapper)
+        {
+            _summarizationRequestRepository = summarizationRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<SummarizationRequestDto>> Handle(GetSummarizationRequestsByStatusQuery request, CancellationToken cancellationToken)
+        {
+            var summarizationRequests = await _summarizationRequestRepository.GetByStatusAsync(request.RequestStatus);
+            return _mapper.Map<List<SummarizationRequestDto>>(summarizationRequests);
+        }
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Queries/SummarizationRequestQueries/GetSummarizationRequestsByStatusQuery.cs
@@ -0,0 +1,12 @@
+﻿
+using AIYTVideoSummarizer.Application.DTOs.SummarizationRequestDtos;
+using AIYTVideoSummarizer.Domain.Enums;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Queries.SummarizationRequestQueries
+{
+    public class GetSummarizationRequestsByStatusQuery:IRequest<List<SummarizationRequestDto>>
+    {
+        public RequestStatus RequestStatus { get; set; }
+    }
+}
