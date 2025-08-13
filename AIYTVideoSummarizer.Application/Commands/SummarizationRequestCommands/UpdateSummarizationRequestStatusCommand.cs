faec515c2122a9ec03219bc1d6844ae794539ed1
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-13 20:33:19 +0300

    Add(application: UpdateSummarizationRequestStatus Command, Handler

---
+++ AIYTVideoSummarizer.Application/Commands/SummarizationRequestCommands/UpdateSummarizationRequestStatusCommand.cs
@@ -0,0 +1,14 @@
+﻿
+using AIYTVideoSummarizer.Application.DTOs.SummarizationRequestDtos;
+using AIYTVideoSummarizer.Domain.Enums;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Commands.SummarizationRequestCommands
+{
+    public class UpdateSummarizationRequestStatusCommand:IRequest<SummarizationRequestDto>
+    {
+        public Guid RequestId { get; set; }
+        public RequestStatus RequestStatus { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Handlers/SummarizationRequestHandlers/UpdateSummarizationRequestStatusCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿
+using AIYTVideoSummarizer.Application.Commands.SummarizationRequestCommands;
+using AIYTVideoSummarizer.Application.DTOs.SummarizationRequestDtos;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.SummarizationRequestHandlers
+{
+    public class UpdateSummarizationRequestStatusCommandHandler : IRequestHandler<UpdateSummarizationRequestStatusCommand, SummarizationRequestDto>
+    {
+        private readonly ISummarizationRequestRepository _summarizationRequestRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateSummarizationRequestStatusCommandHandler(
+            ISummarizationRequestRepository summarizationRequestRepository,
+            IMapper mapper)
+        {
+            _summarizationRequestRepository = summarizationRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<SummarizationRequestDto> Handle(UpdateSummarizationRequestStatusCommand request, CancellationToken cancellationToken)
+        {
+            var summarizationRequest = await _summarizationRequestRepository.GetByIdAsync(request.RequestId);
+            if (summarizationRequest == null)
+                throw new KeyNotFoundException($"Request {request.RequestId} not found.");
+            summarizationRequest.RequestStatus = request.RequestStatus;
+            summarizationRequest.ErrorMessage = request.ErrorMessage;
+            await _summarizationRequestRepository.UpdateAsync(summarizationRequest);
+
+            var updatedRequest = await _summarizationRequestRepository.GetByIdAsync(request.RequestId);
+
+            return _mapper.Map<SummarizationRequestDto>(updatedRequest);
+        }
+    }
+}
