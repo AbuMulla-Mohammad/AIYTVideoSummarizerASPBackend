6fe1ed510a4593022b15b7cd39ed5f6b8839b54d
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-14 21:23:06 +0300

    Add(application): add UpdatePromptCommand and handler

---
+++ AIYTVideoSummarizer.Application/Commands/PromptCommands/UpdatePromptCommand.cs
@@ -0,0 +1,13 @@
+﻿using AIYTVideoSummarizer.Application.DTOs.PromptDtos;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Commands.PromptCommands
+{
+    public class UpdatePromptCommand:IRequest<PromptDto?>
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; } 
+        public string? Text { get; set; } 
+        public string? Description { get; set; }
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Handlers/PromptHandlers/UpdatePromptCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AIYTVideoSummarizer.Application.Commands.PromptCommands;
+using AIYTVideoSummarizer.Application.DTOs.PromptDtos;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.PromptHandlers
+{
+    public class UpdatePromptCommandHandler : IRequestHandler<UpdatePromptCommand, PromptDto?>
+    {
+        private readonly IPromptRepository _promptRepository;
+        private readonly IMapper _mapper;
+
+        public UpdatePromptCommandHandler(
+            IPromptRepository promptRepository,
+            IMapper mapper)
+        {
+            _promptRepository = promptRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PromptDto?> Handle(UpdatePromptCommand request, CancellationToken cancellationToken)
+        {
+            var prompt = await _promptRepository.GetByIdAsync(request.Id);
+            if (prompt is null)
+                return null;
+            if (!string.IsNullOrWhiteSpace(request.Name))
+                prompt.Name = request.Name;
+
+            if (!string.IsNullOrWhiteSpace(request.Text))
+                prompt.Text = request.Text;
+
+            if (!string.IsNullOrWhiteSpace(request.Description))
+                prompt.Description = request.Description;
+
+            await _promptRepository.UpdateAsync(prompt);
+            return _mapper.Map<PromptDto>(prompt);
+
+        }
+    }
+}
