81385e985c718aa01f2b183488d7afc0917fcf9f
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-14 21:56:16 +0300

    Add(domain): add reusable NotFoundException for all entities

---
+++ AIYTVideoSummarizer.Domain/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace AIYTVideoSummarizer.Domain.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(
+            string entityName,
+            object? Id = null) :base(Id!=null
+            ?$"{entityName} with '{Id}' was not found."
+            :$"{entityName} was not found.")
+        {
+        }
+    }
+}
