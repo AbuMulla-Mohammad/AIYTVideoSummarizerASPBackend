4c95cc011da7d524f537d592d546eb384316f18e
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-18 22:02:42 +0300

    Add(application): add GetUserSummarizationRequestsQuery, Handler, Dto and AutoMapper Profile

---
+++ AIYTVideoSummarizer.Application/DTOs/SummarizationRequestDtos/UserSummarizationRequestDto.cs
@@ -0,0 +1,20 @@
+﻿
+using AIYTVideoSummarizer.Domain.Entities;
+using AIYTVideoSummarizer.Domain.Enums;
+
+namespace AIYTVideoSummarizer.Application.DTOs.SummarizationRequestDtos
+{
+    public class UserSummarizationRequestDto
+    {
+        public Guid Id { get; set; } 
+        public DateTime RequestedAt { get; set; }
+        public string RequestStatus { get; set; } = "Pending";
+        public string? ErrorMessage { get; set; }
+
+        public Guid VideoId { get; set; }
+        public string? VideoTitle { get; set; }
+
+        public Guid PromptId { get; set; }
+        public string PromptUsed { get; set; } = string.Empty;
+    }
+}
--- AIYTVideoSummarizer.Application/Profiles/SummarizationRequestProfile.cs
+++ AIYTVideoSummarizer.Application/Profiles/SummarizationRequestProfile.cs
@@ -18,6 +18,10 @@ namespace AIYTVideoSummarizer.Application.Profiles
                            opt => opt.MapFrom(src => src.Prompt.Description))
                 .ForMember(dest => dest.YouTubeUrl,
                            opt => opt.MapFrom(src => src.Video.YouTubeUrl));
+            CreateMap<SummarizationRequest, UserSummarizationRequestDto>()
+                .ForMember(dest => dest.PromptUsed, opt => opt.MapFrom(src => src.Prompt.Name))
+                .ForMember(dest => dest.VideoTitle, opt => opt.MapFrom(src => src.Video.Title))
+                .ForMember(dest => dest.RequestStatus, opt => opt.MapFrom(src => src.RequestStatus.ToString()));
             CreateMap<CreateSummarizationRequestCommand, SummarizationRequest>();
         }
     }
