c82bd3f1fe756e301c4c61d5d1a5cc134676387e
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-19 22:22:21 +0300

    Add(application): add DeleteExternalLoginCommand, Handler

---
+++ AIYTVideoSummarizer.Application/Handlers/UserExternalLoginHandlers/DeleteExternalLoginCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using AIYTVideoSummarizer.Application.Commands.UserExternalLoginCommands;
+using AIYTVideoSummarizer.Domain.Common.Exceptions;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AIYTVideoSummarizer.Domain.Entities;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.UserExternalLoginHandlers
+{
+    public class DeleteExternalLoginCommandHandler : IRequestHandler<DeleteExternalLoginCommand, Unit>
+    {
+        private readonly IUserExternalLoginRepository _userExternalLoginRepository;
+
+        public DeleteExternalLoginCommandHandler(IUserExternalLoginRepository userExternalLoginRepository)
+        {
+            _userExternalLoginRepository = userExternalLoginRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteExternalLoginCommand request, CancellationToken cancellationToken)
+        {
+            var userExternalLogin = await _userExternalLoginRepository.GetByIdAsync(request.Id)
+                ?? throw new NotFoundException(nameof(UserExternalLogin), request.Id);
+            await _userExternalLoginRepository.DeleteAsync(request.Id);
+            return Unit.Value;
+        }
+    }
+}
