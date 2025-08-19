dba381c99417367213acb7ab2aef3cd4a4bbe7c3
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-19 22:11:03 +0300

    Add(application):add CreateExternalLoginCommand,Handler, AutoMapper Profile

---
+++ AIYTVideoSummarizer.Application/Handlers/UserExternalLoginHandlers/CreateExternalLoginCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿
+using AIYTVideoSummarizer.Application.Commands.UserExternalLoginCommands;
+using AIYTVideoSummarizer.Domain.Common.Interfaces.Repositories;
+using AIYTVideoSummarizer.Domain.Entities;
+using AutoMapper;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Handlers.UserExternalLoginHandlers
+{
+    public class CreateExternalLoginCommandHandler : IRequestHandler<CreateExternalLoginCommand, Guid>
+    {
+        private readonly IUserExternalLoginRepository _userExternalLoginRepository;
+        private readonly IMapper _mapper;
+
+        public CreateExternalLoginCommandHandler(
+            IUserExternalLoginRepository userExternalLoginRepository,
+            IMapper mapper)
+        {
+            _userExternalLoginRepository = userExternalLoginRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Guid> Handle(CreateExternalLoginCommand request, CancellationToken cancellationToken)
+        {
+            var userExternalLogin = await _userExternalLoginRepository.AddAsync(_mapper.Map<UserExternalLogin>(request));
+            return userExternalLogin.Id;
+        }
+    }
+}
