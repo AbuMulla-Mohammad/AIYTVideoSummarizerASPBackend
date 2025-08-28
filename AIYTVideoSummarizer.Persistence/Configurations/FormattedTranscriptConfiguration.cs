329e1466a1a22886e0e2755e4e52894e4c838639
AbuMulla-Mohammad <m2872001@gmail.com>
2025-08-28 16:49:21 +0300

    Add(Persistence): add FormattedTranscriptConfiguration

---
+++ AIYTVideoSummarizer.Persistence/Configurations/FormattedTranscriptConfiguration.cs
@@ -0,0 +1,23 @@
+﻿
+using AIYTVideoSummarizer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AIYTVideoSummarizer.Persistence.Configurations
+{
+    public class FormattedTranscriptConfiguration : IEntityTypeConfiguration<FormattedTranscript>
+    {
+        public void Configure(EntityTypeBuilder<FormattedTranscript> builder)
+        {
+            builder.Property(ft => ft.Text)
+                .IsRequired();
+
+            builder.HasIndex(ft => ft.VideoId);
+
+            builder.HasOne(ft => ft.Video)
+                .WithMany(v => v.FormattedTranscripts)
+                .HasForeignKey(ft => ft.VideoId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
