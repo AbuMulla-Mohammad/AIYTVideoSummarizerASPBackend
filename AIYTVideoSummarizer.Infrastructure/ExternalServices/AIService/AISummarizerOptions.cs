912e32896686540e761847d63f0b2133a14551ef
AbuMulla-Mohammad <m2872001@gmail.com>
2025-09-06 21:58:00 +0300

    Add(infrastructure): add AISummarizerOptions, DI registration, appsettings, and required packages

---
+++ AIYTVideoSummarizer.Infrastructure/ExternalServices/AIService/AISummarizerOptions.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AIYTVideoSummarizer.Infrastructure.ExternalServices.AIService
+{
+    public class AISummarizerOptions
+    {
+        public const string SectionName = "AIService";
+        [Required]
+        public string BaseUrl { get; set; } = string.Empty;
+        [Required]
+        public string GetVideoIdEndpoint { get; set; } = "api/get_video_id";
+        [Required]
+        public string SummarizeEndpoint { get; set; } = "api/summarize_format_transcript";
+
+        [Range(1, 300)]
+        public int TimeoutSeconds { get; set; } = 30;
+
+        public int RetryCount { get; set; } = 3;
+        public int CircuitBreakerFailures { get; set; } = 5;
+        public bool UseSandbox { get; set; } = false;
+
+        public Dictionary<string, string>? DefaultHeaders { get; set; }
+    }
+}
---
+++ AIYTVideoSummarizer.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using AIYTVideoSummarizer.Application.Interfaces.ExternalServices;
+using AIYTVideoSummarizer.Infrastructure.ExternalServices.AIService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AIYTVideoSummarizer.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration config)
+        {
+            services.AddOptions<AISummarizerOptions>()
+                .Bind(config.GetSection(AISummarizerOptions.SectionName))
+                .ValidateDataAnnotations()
+                .Validate(o => Uri.IsWellFormedUriString(o.BaseUrl, UriKind.Absolute),
+                        "AIService:BaseUrl must be a valid absolute URI")
+                .ValidateOnStart();
+
+            return services;
+
+        }
+    }
+}
