2d3ced9e700d13f55bc3dfe0bf8a82bb00a563d3
AbuMulla-Mohammad <m2872001@gmail.com>
2025-09-09 20:08:04 +0300

    Add(Infrastructure): add AISummarizerClient and register it in the DI

--- AIYTVideoSummarizer.Infrastructure/InfrastructureServiceRegistration.cs
+++ AIYTVideoSummarizer.Infrastructure/InfrastructureServiceRegistration.cs
@@ -2,6 +2,7 @@
 using AIYTVideoSummarizer.Infrastructure.ExternalServices.AIService;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace AIYTVideoSummarizer.Infrastructure
 {
@@ -15,7 +16,20 @@ namespace AIYTVideoSummarizer.Infrastructure
                 .Validate(o => Uri.IsWellFormedUriString(o.BaseUrl, UriKind.Absolute),
                         "AIService:BaseUrl must be a valid absolute URI")
                 .ValidateOnStart();
+            services.AddHttpClient<IAIService, AISummarizerClient>((provider, client) =>
+            {
+            var options = provider.GetRequiredService<IOptions<AISummarizerOptions>>().Value;
+            client.BaseAddress = new Uri(options.BaseUrl);
+            client.Timeout = TimeSpan.FromSeconds(options.TimeoutSeconds);
 
+                if (options.DefaultHeaders != null)
+                {
+                    foreach (var header in options.DefaultHeaders)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+            });
             return services;
 
         }
