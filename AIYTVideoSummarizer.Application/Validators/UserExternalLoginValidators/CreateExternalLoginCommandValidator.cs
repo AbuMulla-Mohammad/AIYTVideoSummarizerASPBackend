5379ce16e66b430e6254a45d096aaee3fc42288d
AbuMulla-Mohammad <m2872001@gmail.com>
2025-09-23 18:28:57 +0300

    Add(Validation): Add validators for User External Logins queries and commands

---
+++ AIYTVideoSummarizer.Application/Validators/UserExternalLoginValidators/CreateExternalLoginCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿
+using AIYTVideoSummarizer.Application.Commands.UserExternalLoginCommands;
+using FluentValidation;
+
+namespace AIYTVideoSummarizer.Application.Validators.UserExternalLoginValidators
+{
+    public class CreateExternalLoginCommandValidator : AbstractValidator<CreateExternalLoginCommand>
+    {
+        public CreateExternalLoginCommandValidator()
+        {
+            RuleFor(ex => ex.UserId)
+                .NotEmpty().WithMessage("User Id must be provided.");
+
+            RuleFor(ex=>ex.LoginProvider)
+                .NotEmpty().WithMessage("Login provider is required.")
+                .MaximumLength(100).WithMessage("Login provider cannot exceed 100 characters.");
+
+            RuleFor(ex=>ex.ProviderKey)
+                .NotEmpty().WithMessage("Provider key is required.")
+                .MinimumLength(5).WithMessage("Provider key must be at least 5 characters.")
+                .MaximumLength(200).WithMessage("Provider key cannot exceed 200 characters.");
+        }
+    }
+}
