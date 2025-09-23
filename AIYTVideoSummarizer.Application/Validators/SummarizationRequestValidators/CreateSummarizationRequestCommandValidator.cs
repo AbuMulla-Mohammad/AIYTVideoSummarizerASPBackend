21c4e43fd522109b6409db19b3b5e3665f8db0f1
AbuMulla-Mohammad <m2872001@gmail.com>
2025-09-23 21:28:33 +0300

    Add(Validation): Add validators for Summarization Request queries and commands

---
+++ AIYTVideoSummarizer.Application/Validators/SummarizationRequestValidators/CreateSummarizationRequestCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿
+using AIYTVideoSummarizer.Application.Commands.SummarizationRequestCommands;
+using FluentValidation;
+
+namespace AIYTVideoSummarizer.Application.Validators.SummarizationRequestValidators
+{
+    public class CreateSummarizationRequestCommandValidator : AbstractValidator<CreateSummarizationRequestCommand>
+    {
+        public CreateSummarizationRequestCommandValidator()
+        {
+            RuleFor(sr => sr.YouTubeUrl)
+                .NotEmpty().WithMessage("YouTube video URL is required.")
+                .MaximumLength(500).WithMessage("YouTube video URL can't be longer than 500 characters.")
+                .Must(url => Uri.TryCreate(url, UriKind.Absolute, out _))
+                .WithMessage("Invalid URL format.")
+                .Must(url => url.Contains("youtube.com") || url.Contains("youtu.be"))
+                .WithMessage("URL must be a YouTube link.");
+
+            RuleFor(sr => sr.PromptId)
+                .NotEmpty().WithMessage("Prompt Id must be provided.");
+
+            RuleFor(sr => sr.UserId)
+                .NotEmpty().WithMessage("User Id must be provided.");
+
+        }
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Validators/SummarizationRequestValidators/GetSummarizationRequestByIdQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿
+using AIYTVideoSummarizer.Application.Queries.SummarizationRequestQueries;
+using FluentValidation;
+
+namespace AIYTVideoSummarizer.Application.Validators.SummarizationRequestValidators
+{
+    public class GetSummarizationRequestByIdQueryValidator : AbstractValidator<GetSummarizationRequestByIdQuery>
+    {
+        public GetSummarizationRequestByIdQueryValidator()
+        {
+            RuleFor(sr => sr.SummarizationRequestId)
+            .NotEmpty().WithMessage("Summarization Request Id must be provided.");
+        }
+    }
+}
