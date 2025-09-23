4ab50dc6ab172834385450090ab5bc40bd315b0e
AbuMulla-Mohammad <m2872001@gmail.com>
2025-09-23 21:12:00 +0300

    Add(Validation): Add validators for User queries and commands

---
+++ AIYTVideoSummarizer.Application/Validators/UserValidators/CreateUserCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿
+using AIYTVideoSummarizer.Application.Commands.UserCommands;
+using FluentValidation;
+
+namespace AIYTVideoSummarizer.Application.Validators.UserValidators
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("Username is required.")
+                .MinimumLength(5).WithMessage("Username must be at least 5 characters long.")
+                .MaximumLength(70).WithMessage("Username cannot exceed 70 characters.");
+
+            RuleFor(x => x.Email)
+               .NotEmpty().WithMessage("Email is required.")
+               .EmailAddress().WithMessage("A valid email address is required.")
+               .MaximumLength(254).WithMessage("Email cannot exceed 254 characters.");
+
+            RuleFor(x => x.Password)
+               .NotEmpty().WithMessage("Password is required.")
+               .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+               .Must(password => password.Any(char.IsUpper) && password.Any(char.IsLower))
+               .WithMessage("Password must contain both uppercase and lowercase characters.")
+               .Must(password => password.Any(char.IsDigit))
+               .WithMessage("Password must contain at least one digit.")
+               .Must(password => password.Any(ch => !char.IsLetterOrDigit(ch)))
+               .WithMessage("Password must contain at least one special character.");
+        }
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Validators/UserValidators/GetUserByIdQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿
+using AIYTVideoSummarizer.Application.Queries.UserQueries;
+using FluentValidation;
+
+namespace AIYTVideoSummarizer.Application.Validators.UserValidators
+{
+    public class GetUserByIdQueryValidator : AbstractValidator<GetUserByIdQuery>
+    {
+        public GetUserByIdQueryValidator()
+        {
+            RuleFor(u => u.UserId)
+                .NotEmpty().WithMessage("User Id must be provided.");
+        }
+    }
+}
