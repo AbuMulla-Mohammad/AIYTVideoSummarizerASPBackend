ab23bcd947b70c20e73be2710482acbe9d51718d
AbuMulla-Mohammad <m2872001@gmail.com>
2025-10-03 23:20:01 +0300

    Add: add CreateSummarizationRequestDto

---
+++ AIYTVideoSummarizer.Application/DTOs/SummarizationRequestDtos/CreateSummarizationRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIYTVideoSummarizer.Application.DTOs.SummarizationRequestDtos
+{
+    public class CreateSummarizationRequestDto
+    {
+        public string YouTubeUrl { get; set; } = string.Empty;
+        public Guid PromptId { get; set; }
+    }
+}
