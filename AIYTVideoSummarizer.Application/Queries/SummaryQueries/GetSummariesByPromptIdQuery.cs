7d90bfa73e1b93cc007d2b6479fd148dfb7d0b02
AbuMulla-Mohammad <m2872001@gmail.com>
2025-10-05 23:08:40 +0300

    Add(Application): add GetSummariesByPromptIdQuery, and Handler

---
+++ AIYTVideoSummarizer.Application/Queries/SummaryQueries/GetSummariesByPromptIdQuery.cs
@@ -0,0 +1,11 @@
+﻿
+using AIYTVideoSummarizer.Application.DTOs.SummaryDtos;
+using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Queries.SummaryQueries
+{
+    public class GetSummariesByPromptIdQuery:IRequest<List<SummaryDto>>
+    {
+        public Guid PromptId { get; set; }
+    }
+}
