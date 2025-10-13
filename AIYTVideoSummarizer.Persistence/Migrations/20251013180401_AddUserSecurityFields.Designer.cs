d4a9e14d81b23b07e8ae62b1a722878086584ec0
AbuMulla-Mohammad <m2872001@gmail.com>
2025-10-13 21:18:20 +0300

    Add(Authentication): add new User fileds (Salt, IsEmailConfirmed, EmailConfirmationToken, PasswordResetToken, PasswordResetTokenExpiry, and CreatedAt) and Configure the new fields and migrate them.

--- AIYTVideoSummarizer.Persistence/Migrations/20251013180401_AddUserSecurityFields.Designer.cs
+++ AIYTVideoSummarizer.Persistence/Migrations/20251013180401_AddUserSecurityFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AIYTVideoSummarizer.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AIYTVideoSummarizer.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251013180401_AddUserSecurityFields")]
+    partial class AddUserSecurityFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
