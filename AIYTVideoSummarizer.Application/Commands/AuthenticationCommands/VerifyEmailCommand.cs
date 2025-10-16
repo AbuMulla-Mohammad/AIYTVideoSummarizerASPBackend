1afdafdeed09fd6d8a757fd3816f83e7cdc1c633
AbuMulla-Mohammad <m2872001@gmail.com>
2025-10-16 20:07:08 +0300

    Add(Auth): add VerifyEmailCommand, Handler, and Validator

---
+++ AIYTVideoSummarizer.Application/Commands/AuthenticationCommands/VerifyEmailCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace AIYTVideoSummarizer.Application.Commands.AuthenticationCommands
+{
+    public class VerifyEmailCommand:IRequest<Unit>
+    {
+        public string Token { get; set; } = string.Empty;
+    }
+}
---
+++ AIYTVideoSummarizer.Application/Validators/AuthenticationValidators/VerifyEmailCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using AIYTVideoSummarizer.Application.Commands.AuthenticationCommands;
+using FluentValidation;
+
+namespace AIYTVideoSummarizer.Application.Validators.AuthenticationValidators
+{
+    public class VerifyEmailCommandValidator : AbstractValidator<VerifyEmailCommand>
+    {
+        public VerifyEmailCommandValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotEmpty().WithMessage("Verification token is required.")
+                .Length(10, 500).WithMessage("Invalid verification token length.");
+        }
+    }
+}
